be599454411c728e6d0562039fa02e749beb4d4a
Sameer Deshmukh <sameer.deshmukh93@gmail.com>
2018-08-15 17:04:57 +0900

    Added more rake tasks for xnd.

--- xnd/ext/ruby_xnd/extconf.rb
+++ xnd/ext/ruby_xnd/extconf.rb
@@ -4,9 +4,10 @@ require 'mkmf'
   find_library(lib, nil, "/home/sameer/gitrepos/plures-ruby/build/lib/")
 end
 
-
 ["xnd.h", "ndtypes.h", "gumath.h"].each do |header|
   find_header(header, "/home/sameer/gitrepos/plures-ruby/build/include")
   have_header(header)
 end
+
+$CFLAGS += " -g "
 create_makefile("ruby_xnd/ruby_xnd")
--- xnd/ext/ruby_xnd/ruby_xnd.c
+++ xnd/ext/ruby_xnd/ruby_xnd.c
@@ -3,4 +3,7 @@
 void Init_ruby_xnd(void)
 {
   VALUE xnd = rb_define_class("XND", rb_cObject);
+
+  char s[3];
+  s[4] = 44;
 }
